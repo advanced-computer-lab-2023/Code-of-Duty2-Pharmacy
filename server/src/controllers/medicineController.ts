8a8d2eb557cad36431f197061e3a01bfad5f075e
SUPEREL1TE <mahmoudaboueleneen@gmail.com>
2023-10-06 17:16:59 +0300

    Added medicineController

--- server/src/controllers/medicineController.ts
+++ server/src/controllers/medicineController.ts
@@ -1,7 +1,7 @@
 import { Request, Response } from 'express';
 import Medicine, { IMedicineModel } from '../models/Medicine';
 
-export const getAllMedicines = async (req: Request, res: Response): Promise<void> => {
+export const getAllMedicines = async (req: Request, res: Response) => {
     try {
         const allMedicines: IMedicineModel[] = await Medicine.find();
         res.json(allMedicines);
