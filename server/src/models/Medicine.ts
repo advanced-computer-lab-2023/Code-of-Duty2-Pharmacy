0c7dbfd80e412afd19c7ea084c1c78e58b0cac6a
SUPEREL1TE <mahmoudaboueleneen@gmail.com>
2023-10-06 16:49:00 +0300

    Added Medicine model

---
+++ server/src/models/Medicine.ts
@@ -0,0 +1,31 @@
+import mongoose, { Document, Schema } from 'mongoose';
+
+export interface IMedicine {
+    name: string;
+    activeIngredients: string[];
+    price: number;
+    availableQuantity: number;
+    pictureUrl?: string;
+    description?: string;
+    usages?: string[];
+    isOverTheCounter?: boolean;
+    isArchived?: boolean;
+}
+
+export interface IMedicineModel extends IMedicine, Document {}
+
+const MedicineSchema: Schema = new Schema({
+    name: { type: String, required: true },
+    activeIngredients: { type: [String], required: true },
+    price: { type: Number, required: true },
+    availableQuantity: { type: Number, required: true },
+    pictureUrl: { type: String },
+    description: { type: String },
+    usages: { type: [String] },
+    isOverTheCounter: { type: Boolean },
+    isArchived: { type: Boolean }
+}, 
+    { timestamps: true }
+);
+
+export default mongoose.model<IMedicineModel>('Medicine', MedicineSchema);
