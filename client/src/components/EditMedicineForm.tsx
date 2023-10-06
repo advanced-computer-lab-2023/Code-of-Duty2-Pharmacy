3e1830c8ded5ad8bb19f6edd8694252a215e3e03
SUPEREL1TE <mahmoudaboueleneen@gmail.com>
2023-10-07 01:34:53 +0300

    Added frontend for editing medicines on Pharmacist page

---
+++ client/src/components/EditMedicineForm.tsx
@@ -0,0 +1,85 @@
+import { useState } from "react";
+import axios from "axios";
+import config from "../config/config";
+import { Medicine } from "../types";
+import TextField from "@mui/material/TextField";
+import Dialog from "@mui/material/Dialog";
+import DialogTitle from "@mui/material/DialogTitle";
+import DialogContent from "@mui/material/DialogContent";
+import DialogActions from "@mui/material/DialogActions";
+import Button from "@mui/material/Button";
+
+interface Props {
+  medicine: Medicine;
+  open: boolean;
+  onClose: () => void;
+  onUpdated: () => void;
+}
+
+const EditMedicineForm: React.FC<Props> = ({
+  medicine,
+  open,
+  onClose,
+  onUpdated,
+}) => {
+  const [name, setName] = useState(medicine.name);
+  const [price, setPrice] = useState(medicine.price);
+  const [availableQuantity, setAvailableQuantity] = useState(
+    medicine.availableQuantity
+  );
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      await axios.put(`${config.API_URL}/medicines/${medicine._id}`, {
+        name,
+        price,
+        availableQuantity,
+      });
+      onUpdated();
+      onClose();
+    } catch (err) {
+      console.error(err);
+    }
+  };
+
+  return (
+    <Dialog open={open} onClose={onClose}>
+      <DialogTitle>Edit Medicine</DialogTitle>
+      <DialogContent>
+        <br></br>
+        <form id="edit-form" onSubmit={handleSubmit}>
+          <TextField
+            label="Name"
+            value={name}
+            onChange={(e) => setName(e.target.value)}
+          />
+          <br></br>
+          <br></br>
+          <TextField
+            label="Price"
+            type="number"
+            value={price}
+            onChange={(e) => setPrice(Number(e.target.value))}
+          />
+          <br></br>
+          <br></br>
+          <TextField
+            label="Available Quantity"
+            type="number"
+            value={availableQuantity}
+            onChange={(e) => setAvailableQuantity(Number(e.target.value))}
+          />
+        </form>
+      </DialogContent>
+      <DialogActions>
+        <Button onClick={onClose}>Cancel</Button>
+        <Button type="submit" form="edit-form">
+          Update Medicine
+        </Button>
+      </DialogActions>
+    </Dialog>
+  );
+};
+
+export default EditMedicineForm;
--- client/src/components/MedicineList.tsx
+++ client/src/components/MedicineList.tsx
@@ -1,20 +1,46 @@
+import { useState } from "react";
 import { Medicine } from "../types";
+import EditMedicineForm from "./EditMedicineForm";
 
 interface Props {
   medicines: Medicine[];
+  onUpdated: () => void;
 }
 
-const MedicineList: React.FC<Props> = ({ medicines }) => (
-  <div>
-    {medicines.map((medicine) => (
-      <div key={medicine._id}>
-        <img src={medicine.pictureUrl} alt={medicine.name} />
-        <h2>Name: {medicine.name}</h2>
-        <p>Description: {medicine.description}</p>
-        <p>Price: {medicine.price}</p>
-      </div>
-    ))}
-  </div>
-);
+const MedicineList: React.FC<Props> = ({ medicines, onUpdated }) => {
+  const [selectedMedicine, setSelectedMedicine] = useState<Medicine | null>(
+    null
+  );
+
+  const handleEditClick = (medicine: Medicine) => {
+    setSelectedMedicine(medicine);
+  };
+
+  const handleClose = () => {
+    setSelectedMedicine(null);
+  };
+
+  return (
+    <div>
+      {medicines.map((medicine) => (
+        <div key={medicine._id}>
+          <img src={medicine.pictureUrl} alt={medicine.name} />
+          <h2>Name: {medicine.name}</h2>
+          <p>Description: {medicine.description}</p>
+          <p>Price: {medicine.price}</p>
+          <button onClick={() => handleEditClick(medicine)}>Edit</button>
+          {selectedMedicine && selectedMedicine._id === medicine._id && (
+            <EditMedicineForm
+              open={!!selectedMedicine}
+              medicine={selectedMedicine}
+              onClose={handleClose}
+              onUpdated={onUpdated}
+            />
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default MedicineList;
