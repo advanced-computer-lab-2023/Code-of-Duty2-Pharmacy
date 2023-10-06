c9a7c89e87033d22325c39c0865e21b638e60339
SUPEREL1TE <mahmoudaboueleneen@gmail.com>
2023-10-06 21:39:52 +0300

    Updated server adding cors and updated backend config file

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -5,7 +5,6 @@ dotenv.config();
 const config = {
   MONGO_URI: process.env.MONGO_URI || '',
   SERVER_PORT: process.env.SERVER_PORT || '3000',
-  // Add other configuration values as needed in the future
 };
 
 export default config;
