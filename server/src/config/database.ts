71a15ee936273350be5278b5d7e3da88ed54648c
SUPEREL1TE <mahmoudaboueleneen@gmail.com>
2023-10-06 15:50:43 +0300

    Setup DB Config

--- server/src/config/database.ts
+++ server/src/config/database.ts
@@ -8,7 +8,6 @@ const MongoURI: string = process.env.MONGO_URI as string;
 const connectToDB = async () => {
   try {
     await mongoose.connect(MongoURI);
-    // console.log(MongoURI);
     console.log('MongoDB is now connected.');
   } catch (err) {
     console.error(err);
