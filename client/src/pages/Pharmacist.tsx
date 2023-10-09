1daf44b6fe283256633aca8781bf039b916814a5
MoTammaa <mohamed.abotammaa@student.guc.edu.eg>
2023-10-09 03:35:53 +0300

    added to main pharmacist page

--- client/src/pages/Pharmacist.tsx
+++ client/src/pages/Pharmacist.tsx
@@ -6,6 +6,7 @@ import config from "../config/config";
 import { Medicine } from "../types";
 import MedicineList from "../components/MedicineList";
 import AddMedicineForm from "../components/AddMedicineForm";
+import NameSearchBar from "../components/NameSearchBar";
 
 const PharmacistPage: React.FC = () => {
   const [medicines, setMedicines] = useState<Medicine[]>([]);
@@ -29,15 +30,41 @@ const PharmacistPage: React.FC = () => {
     fetchMedicines();
   };
 
+  const goSearch = async (searchTerm: string, searchCollection: string) => {
+    // console.log("search");
+    try {
+      const response = await axios.get(
+        `${config.API_URL}/${searchCollection}/search/?name=${searchTerm}`
+      );
+      console.log("searching for " + searchTerm + " in " + searchCollection);
+      console.log(response.data);
+      setMedicines(response.data);
+    } catch (err: any) {
+      if (err.response?.status === 400) {
+        console.log("Get All Meds");
+        fetchMedicines();
+        return;
+      }
+      console.log(
+        err.response?.status === 404 ? "No Medicine with this name" : err
+      );
+    }
+  };
+
   return (
     <div>
       <h1>Pharmacist Page</h1>
+      <NameSearchBar
+        searchCollection="medicines"
+        onSearch={goSearch}
+        initialValue="(or leave empty for all)"
+      />
+      <AddMedicineForm onMedicineAdded={fetchMedicines} />
       <MedicineList
         medicines={medicines}
         onUpdated={handleUpdated}
         canEdit={true}
       />
-      <AddMedicineForm onMedicineAdded={fetchMedicines} />
     </div>
   );
 };
