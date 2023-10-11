1c1d9006bf1abe5ba2da3dc6e71af843731f9c2e
Ahmedsherif74 <ahmedsherifsaid2000@gmail.com>
2023-10-11 17:39:14 +0300

    Filter Button Updated

--- client/src/components/MedicineList.tsx
+++ client/src/components/MedicineList.tsx
@@ -10,7 +10,6 @@ interface Props {
   canEdit: boolean;
 }
 
-
 const MedicineList: React.FC<Props> = ({
   medicines,
   onUpdated,
@@ -35,21 +34,26 @@ const MedicineList: React.FC<Props> = ({
   };
 
   const filteredMedicines = usageFilter
-  ? medicines.filter((medicine) =>
-      medicine.usages ? medicine.usages.includes(usageFilter) : false
-    )
-  : medicines;
-
+    ? medicines.filter((medicine) =>
+        medicine.usages ? medicine.usages.includes(usageFilter) : false
+      )
+    : medicines;
 
   return (
     <div>
-      <input type="text" value={usageFilter || ''} onChange={handleUsageFilterChange} placeholder="Filter by usage" />
+      <input type="text" value={usageFilter || ''} onChange={handleUsageFilterChange} placeholder="Filter by usage" style={{ width: '300px' , height:'25px' }} />
       {filteredMedicines.length === 0 && <p>No medicines found.</p>}
       {filteredMedicines.map((medicine) => (
         <div key={medicine._id}>
           <h2>Name: {medicine.name}</h2>
           <img src={medicine.pictureUrl} alt={medicine.name} width="200" />
           <p>Description: {medicine.description}</p>
+          <p>Usages:</p>
+          <ul>
+            {medicine.usages && medicine.usages.map((usage, index) => (
+              <li key={index}>{usage}</li>
+            ))}
+          </ul>
           <p>Price: {medicine.price}</p>
           <p>Quantity: {medicine.availableQuantity}</p>
           <p>Sales: {medSales[medicine._id] || 0}</p>
