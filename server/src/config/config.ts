ccbdcf5c1babe700ae4447e2831ac961c2df2d23
Ahmedsherif74 <ahmedsherifsaid2000@gmail.com>
2023-10-14 22:08:54 +0300

    Cors Error Handled

--- server/src/config/config.ts
+++ server/src/config/config.ts
@@ -5,7 +5,10 @@ const config = {
   MONGO_URI: process.env.MONGO_URI || '',
   SERVER_PORT: process.env.SERVER_PORT || '3000',
   FRONT_END_URL: process.env.FRONT_END_URL || 'http://localhost:5137',
-  corsOptions: { origin: new RegExp(`^${process.env.FRONT_END_URL}`) }
-};
-
+  corsOptions: {
+    origin: 'http://localhost:5173', // or '*' to allow all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }
+}
 export default config;
