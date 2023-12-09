e175baeac148d86914d4746b0907c9d1e44f623d
SUPEREL1TE <mahmoudaboueleneen@gmail.com>
2023-12-09 21:44:42 +0200

    Add todo

--- server/src/models/prescriptions/Prescription.ts
+++ server/src/models/prescriptions/Prescription.ts
@@ -3,6 +3,7 @@ import { IPrescription } from "./interfaces/IPrescription";
 
 export interface IPrescriptionModel extends IPrescription, Document {}
 
+// TODO: Fix interface not being enforced for some reason.
 export const PrescriptionSchema = new Schema<IPrescriptionModel>(
   {
     doctorId: { type: Schema.Types.ObjectId, ref: "Doctor", required: true },
