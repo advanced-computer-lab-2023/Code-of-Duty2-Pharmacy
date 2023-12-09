c446a27446ef8b3d5d7326d7d65530f24b388b81
MoTammaa <mohamed.abotammaa@student.guc.edu.eg>
2023-12-10 01:32:15 +0200

    unused imports

--- client/src/components/medicine/MedicineCard.tsx
+++ client/src/components/medicine/MedicineCard.tsx
@@ -1,4 +1,4 @@
-import React, { Ref, forwardRef, useState } from "react";
+import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import Card from "@mui/material/Card";
