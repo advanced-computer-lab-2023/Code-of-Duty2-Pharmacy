c37f76036c823cd81ebcfca39472963d3441cb29
Mahmoud M. Abou Eleneen <mahmoudaboueleneen@gmail.com>
2023-12-15 20:31:27 +0200

    Add home page new UI

--- client/src/pages/guest/Welcome.tsx
+++ client/src/pages/guest/Welcome.tsx
@@ -1,71 +1,60 @@
-import { useNavigate } from "react-router-dom";
-import Button from "@mui/material/Button";
-import Box from "@mui/material/Box";
-import Typography from "@mui/material/Typography";
+import React, { useEffect, useState } from "react";
+import { Box, Container } from "@mui/material";
+import { motion } from "framer-motion";
 
-import { patientRegistrationRoute } from "../../data/routes/guestRoutes";
-import { pharmacistRegistrationRoute } from "../../data/routes/guestRoutes";
-import { adminDashboardRoute } from "../../data/routes/adminRoutes";
-import { pharmacistDashboardRoute } from "../../data/routes/pharmacistRoutes";
-import { patientDashboardRoute } from "../../data/routes/patientRoutes";
+const Home: React.FC = () => {
+  const sections = Array.from({ length: 6 }, (_, i) => {
+    const [ref, setRef] = useState(null);
+    const [opacity, setOpacity] = useState(0);
 
-const Welcome = () => {
-  const navigate = useNavigate();
+    useEffect(() => {
+      const observer = new IntersectionObserver(
+        ([entry]) => {
+          setOpacity(entry.intersectionRatio);
+        },
+        {
+          threshold: Array.from({ length: 101 }, (_, i) => i * 0.01)
+        }
+      );
+
+      if (ref) {
+        observer.observe(ref);
+      }
+
+      return () => {
+        if (ref) {
+          observer.unobserve(ref);
+        }
+      };
+    }, [ref]);
+
+    return (
+      <motion.div style={{ opacity }} transition={{ duration: 1 }} key={i}>
+        <Box
+          ref={setRef}
+          sx={{
+            width: "100%",
+            padding: 2
+          }}
+          id={`section${i + 1}`}
+        >
+          <h2>{`Section ${i + 1}`}</h2>
+          <p>{`This is section ${i + 1}.`}</p>
+        </Box>
+      </motion.div>
+    );
+  });
 
   return (
-    <>
-      <Box sx={{ m: 4 }}>
-        <Typography variant="h4" gutterBottom>
-          Welcome
-        </Typography>
-
-        {/* <Button
-          variant="contained"
-          color="primary"
-          onClick={() => navigate(patientRegistrationRoute.path)}
-          sx={{ m: 1 }}
-        >
-          Register as Patient
-        </Button>
-
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => navigate(pharmacistRegistrationRoute.path)}
-          sx={{ m: 1 }}
-        >
-          Register as Pharmacist
-        </Button>
-
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => navigate(adminDashboardRoute.path)}
-          sx={{ m: 1 }}
-        >
-          Login as Administrator
-        </Button>
-
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => navigate(pharmacistDashboardRoute.path)}
-          sx={{ m: 1 }}
-        >
-          Login as Pharmacist
-        </Button>
-
-        <Button
-          variant="contained"
-          color="primary"
-          onClick={() => navigate(patientDashboardRoute.path)}
-          sx={{ m: 1 }}
-        >
-          Login as Patient
-        </Button> */}
-      </Box>
-    </>
+    <Box
+      sx={{
+        width: "100%",
+        height: "100%"
+      }}
+    >
+      <Container maxWidth="lg">{sections}</Container>
+    </Box>
   );
 };
 
-export default Welcome;
+export default Home;
