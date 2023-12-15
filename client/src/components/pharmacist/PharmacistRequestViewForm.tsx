14463716dc4326c89dc6900aab061d1da4632f45
Mahmoud M. Abou Eleneen <mahmoudaboueleneen@gmail.com>
2023-12-15 23:58:31 +0200

    Update requests UI

--- client/src/components/pharmacist/PharmacistRequestViewForm.tsx
+++ client/src/components/pharmacist/PharmacistRequestViewForm.tsx
@@ -5,24 +5,20 @@ import config from "../../config/config";
 import axios from "axios";
 import { ArrowBack } from "@mui/icons-material";
 import Box from "@mui/material/Box";
+import { Typography } from "@mui/material";
 
 const ViewPharmacistRequestForm = () => {
   const navigate = useNavigate();
 
   const params = new URLSearchParams(window.location.search);
-  const request = JSON.parse(
-    params.get("request") as string | ""
-  ) as PharmacistRegistrationRequest;
+  const request = JSON.parse(params.get("request") as string | "") as PharmacistRegistrationRequest;
   console.log(request);
 
   const handleAccept = (username: string) => {
     axios
-      .post(
-        `${config.API_URL}/pharmacist-registration-requests/accept-pharmacist-request`,
-        {
-          username: username,
-        }
-      )
+      .post(`${config.API_URL}/pharmacist-registration-requests/accept-pharmacist-request`, {
+        username: username
+      })
       .then((response) => {
         if (response.status === 200) {
           navigate(`/admin/pharmacist-registration-requests`);
@@ -31,12 +27,9 @@ const ViewPharmacistRequestForm = () => {
   };
   const handleReject = (username: string) => {
     axios
-      .post(
-        `${config.API_URL}/pharmacist-registration-requests/reject-pharmacist-request`,
-        {
-          username: username,
-        }
-      )
+      .post(`${config.API_URL}/pharmacist-registration-requests/reject-pharmacist-request`, {
+        username: username
+      })
       .then((response) => {
         if (response.status === 200) {
           navigate(`/admin/pharmacist-registration-requests`);
@@ -53,7 +46,9 @@ const ViewPharmacistRequestForm = () => {
       >
         Back To Requests
       </Button>
-      <h1>View Request</h1>
+      <Typography my={3} variant="h4" gutterBottom component="div" color="primary">
+        View Request
+      </Typography>
       <Box
         sx={{
           border: "1px solid #ddd", // Border color
@@ -62,7 +57,7 @@ const ViewPharmacistRequestForm = () => {
           borderRadius: "18px", // Border radius
           display: "grid",
           gridTemplateColumns: { md: "1fr 1fr" },
-          gap: 2,
+          gap: 2
         }}
       >
         <p style={{ margin: "0", fontSize: "1.2rem" }}>
@@ -75,8 +70,7 @@ const ViewPharmacistRequestForm = () => {
           <strong>Email:</strong> {request.email}
         </p>
         <p style={{ margin: "0", fontSize: "1.2rem" }}>
-          <strong>Date of Birth:</strong>{" "}
-          {new Date(request.dateOfBirth).toLocaleDateString()}
+          <strong>Date of Birth:</strong> {new Date(request.dateOfBirth).toLocaleDateString()}
         </p>
         <p style={{ margin: "0", fontSize: "1.2rem" }}>
           <strong>Hourly Rate:</strong> {request.hourlyRate}
@@ -85,8 +79,7 @@ const ViewPharmacistRequestForm = () => {
           <strong>Affiliation:</strong> {request.affiliation}
         </p>
         <p style={{ margin: "0", fontSize: "1.2rem" }}>
-          <strong>Educational Background:</strong>{" "}
-          {request.educationalBackground}
+          <strong>Educational Background:</strong> {request.educationalBackground}
         </p>
 
         <p style={{ margin: "0", fontSize: "1.2rem" }}>
@@ -98,24 +91,17 @@ const ViewPharmacistRequestForm = () => {
       <div
         style={{
           display: "flex",
-          justifyContent: "center", // Center horizontally
+          justifyContent: "center" // Center horizontally
         }}
       >
         <div style={{ paddingRight: "1.0rem", display: "inline" }}>
-          <Button
-            variant="outlined"
-            color="success"
-            onClick={() => handleAccept(request.username)}
-          >
-            Accept
+          <Button variant="outlined" color="error" onClick={() => handleReject(request.username)}>
+            Reject
           </Button>
         </div>
-        <Button
-          variant="outlined"
-          color="error"
-          onClick={() => handleReject(request.username)}
-        >
-          Reject
+
+        <Button variant="outlined" color="success" onClick={() => handleAccept(request.username)}>
+          Accept Request
         </Button>
       </div>
     </div>
