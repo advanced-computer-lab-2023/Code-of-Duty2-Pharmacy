8b93d37f7e0201b53bf6798c680fec5729168e1e
Mahmoud M. Abou Eleneen <mahmoudaboueleneen@gmail.com>
2023-12-16 11:04:33 +0200

    feature: view current and past orders

--- client/src/components/order/OrderList.tsx
+++ client/src/components/order/OrderList.tsx
@@ -4,6 +4,8 @@ import config from "../../config/config";
 
 import OrderCard from "./OrderCard";
 import { Order } from "../../types";
+import { Typography } from "@mui/material";
+import { Box } from "@mui/system";
 
 interface Props {
   canViewStatus: boolean;
@@ -34,17 +36,39 @@ const OrderList: React.FC<Props> = ({ canViewStatus }) => {
     }
   };
 
+  const successfulOrders = orders.filter((order) => order.orderStatus === "successful");
+  const unsuccessfulOrders = orders.filter((order) => order.orderStatus !== "successful");
+
   return (
-    <div>
-      {orders.map((order, index) => (
-        <OrderCard
-          key={index}
-          order={order}
-          canViewStatus={canViewStatus}
-          onCancel={cancelOrder}
-        />
-      ))}
-    </div>
+    <>
+      {successfulOrders.length > 0 && (
+        <>
+          <Box ml={5}>
+            <Typography variant="h4" gutterBottom component="div" color="primary">
+              Current Orders
+            </Typography>
+          </Box>
+
+          {successfulOrders.map((order, index) => (
+            <OrderCard key={index} order={order} canViewStatus={canViewStatus} onCancel={cancelOrder} />
+          ))}
+        </>
+      )}
+
+      {unsuccessfulOrders.length > 0 && (
+        <>
+          <Box ml={5}>
+            <Typography variant="h4" gutterBottom component="div" color="primary">
+              Past Orders
+            </Typography>
+          </Box>
+
+          {unsuccessfulOrders.map((order, index) => (
+            <OrderCard key={index} order={order} canViewStatus={canViewStatus} onCancel={cancelOrder} />
+          ))}
+        </>
+      )}
+    </>
   );
 };
 
