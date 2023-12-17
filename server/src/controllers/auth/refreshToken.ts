d3ed10df794fa99472a208c3f14a1b4f524a5164
Mahmoud M. Abou Eleneen <mahmoudaboueleneen@gmail.com>
2023-12-17 04:27:23 +0200

    Update ports, fix bug in Notif Context

--- server/src/controllers/auth/refreshToken.ts
+++ server/src/controllers/auth/refreshToken.ts
@@ -1,32 +1,25 @@
 import { Response } from "express";
 import { StatusCodes } from "http-status-codes";
-import {
-  signAndGetAccessToken,
-  verifyAndDecodeRefreshToken,
-} from "../../utils/jwt";
+import { signAndGetAccessToken, verifyAndDecodeRefreshToken } from "../../utils/jwt";
 import { AuthorizedRequest } from "../../types/AuthorizedRequest";
 
 export const refreshAccessToken = (req: AuthorizedRequest, res: Response) => {
   const refreshToken = req.cookies?.refreshToken;
 
   if (!refreshToken) {
-    return res
-      .status(StatusCodes.UNAUTHORIZED)
-      .json({ message: "Refresh token is missing" });
+    return res.status(StatusCodes.UNAUTHORIZED).json({ message: "Refresh token is missing" });
   }
 
   try {
     const decodedToken = verifyAndDecodeRefreshToken(refreshToken);
     const accessToken = signAndGetAccessToken({
       id: decodedToken.id,
-      role: decodedToken.role,
+      role: decodedToken.role
     });
     res.status(StatusCodes.OK).json({ accessToken, role: decodedToken.role });
   } catch (error) {
     res.clearCookie("refreshToken", { httpOnly: true });
     req.user = undefined;
-    res
-      .status(StatusCodes.UNAUTHORIZED)
-      .json({ message: "Refresh token is invalid" });
+    res.status(StatusCodes.UNAUTHORIZED).json({ message: "Refresh token is invalid" });
   }
 };
